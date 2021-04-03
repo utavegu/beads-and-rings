8d0bb1d317121e2b0a2624c5db6ac3401234c2c3
Utavegu <utavegu@list.ru>
2021-04-03 20:41:16 +0500

    Получше разобрался с CSS-modules и навёл красоту

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,7 @@
 import React from 'react';
-import styles from './Footer.module.css';
+import s from './Footer.module.css';
 
 export default function Footer() {
-  /* Так, вроде какая-то библиотека есть, чтобы такое безобразие не разводить... classnames... да не сильно лучше, честно говоря. Но пока так */
-  // Платёжные системы
-  const paypal = styles.footerPaySystems + ' ' + styles.footerPaySystemsPaypal;
-  const masterCard = styles.footerPaySystems + ' ' + styles.footerPaySystemsMasterCard;
-  const visa = styles.footerPaySystems + ' ' +  styles.footerPaySystemsVisa;
-  const yandex = styles.footerPaySystems + ' ' +  styles.footerPaySystemsYandex;
-  const webmoney = styles.footerPaySystems + ' ' +  styles.footerPaySystemsWebmoney;
-  const qiwi = styles.footerPaySystems + ' ' +  styles.footerPaySystemsQiwi;
-  // Социальные сети
-  const twitter = styles.footerSocialLink + ' ' + styles.footerSocialLinkTwitter;
-  const vk = styles.footerSocialLink + ' ' + styles.footerSocialLinkVk;
-
   return (
     <footer className="container bg-light footer">
 
@@ -37,21 +25,21 @@ export default function Footer() {
         <a className="footer-contacts-phone" href="tel:+7-123-456-89-01">+7-123-456-89-01</a>
         <span className="footer-contacts-working-hours">Ежедневно: с 09-00 до 18-00</span>
         <a className="footer-contacts-email" href="mailto:testmail@mail.ru">testmail@mail.ru</a>
-        <div className="footerSocialLinks">
-          <div className={twitter}></div>
-          <div className={vk}></div>
+        <div className={s.social_links}>
+          <div className={`${s.social_link} ${s.social_link__twitter}`}></div>
+          <div className={`${s.social_link} ${s.social_link__vk}`}></div>
         </div>
       </section>
 
       <section>
         <h5>Принимаем к оплате:</h5>
-        <div className={styles.footerPay}>
-          <div className={paypal}></div>
-          <div className={masterCard}></div>
-          <div className={visa}></div>
-          <div className={yandex}></div>
-          <div className={webmoney}></div>
-          <div className={qiwi}></div>
+        <div className={s.pay}>
+          <div className={`${s.pay_systems} ${s.pay_systems__paypal}`}></div>
+          <div className={`${s.pay_systems} ${s.pay_systems__mastercard}`}></div>
+          <div className={`${s.pay_systems} ${s.pay_systems__visa}`}></div>
+          <div className={`${s.pay_systems} ${s.pay_systems__yandex}`}></div>
+          <div className={`${s.pay_systems} ${s.pay_systems__webmoney}`}></div>
+          <div className={`${s.pay_systems} ${s.pay_systems__qiwi}`}></div>
         </div>
       </section>
 
