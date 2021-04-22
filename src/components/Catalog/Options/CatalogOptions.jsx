d427b231567b0cd8678dc7b3cb1ac7b6bda89b25
Utavegu <utavegu@list.ru>
2021-04-22 20:26:14 +0500

    Начал оживлять фильтры каталога

--- src/components/Catalog/Options/CatalogOptions.jsx
+++ src/components/Catalog/Options/CatalogOptions.jsx
@@ -1,27 +1,108 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from '../../Button';
-import s from './CatalogOptions.module.css'; // Хм... можно сам компонент не разбивать, а стили для него попробовать сделать разные
+import s from './CatalogOptions.module.css';
 import RangeSlider from './RangeSlider.jsx';
 
 export default function CatalogOptions() {
+
+  /*
+  const [options, setOptions] = useState({
+    presentation: {
+      cards: true,
+      list: false,
+    },
+    category: {
+      rings: true,
+      beads: true,
+    },
+    budget: {
+      min: null,
+      max: null,
+    }
+  })
+  */
+
+
+  // Вот эти 4 - 1 общий объект (смотри выше)
+  const [presentation, setPresentation] = useState({
+    activeView: "cards",
+  })
+
+  const [category, setCategory] = useState({
+    rings: true,
+    beads: true,
+  })
+
+  const [budget, setBuget] = useState({
+    min: null,
+    max: null,
+  })
+
+  const [sort, setSort] = useState({
+    activeSort: "name-ascending",
+  })
+
+
+
+  // А эти 2 функции собираешь в одну и универсализируешь с помощью свитч-кейса
+  const handleChange = ({target}) => {
+    const name = target.name;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    setCategory(prevForm => ({...prevForm, [name]: value}));
+    // setBuget(prevForm => ({...prevForm, [name]: value}));
+  }
+
+  const checkOnlyOne = ({target}) => {
+    // setPresentation({activeView: target.name});
+    setSort({activeSort: target.name});
+  };
+
+
+
+  const handleSubmit = evt => {
+    evt.preventDefault();
+
+    // handleSend(options);
+
+    // сброс не понадобится
+    // setOptions({
+    //   presentation: '',
+    //   category: '',
+    //   budget: '',
+    //   sort: '',
+    // })
+  }
+
   return (
     <section className={s.options}>
 
-      <form action="">
+      <form onSubmit={handleSubmit}>
 
         <fieldset className={s.presentation}>
           <legend>Представление</legend>
           <ul>
             <li>
               <label>
-                <input className="visually-hidden" type="radio" name="presentation" value="Карточки"/>
+                <input
+                  className="visually-hidden"
+                  type="radio"
+                  name="cards"
+                  checked={presentation.activeView === "cards"}
+                  onChange={checkOnlyOne}
+                />
                 <span className={`${s.custom_radio} ${s.list}`} title="Карточки"></span>
                 <span className="visually-hidden">Карточки</span>
               </label>
             </li>
             <li>
               <label>
-                <input className="visually-hidden" type="radio" name="presentation" value="Список"/>
+                <input
+                  className="visually-hidden"
+                  type="radio"
+                  name="list"
+                  checked={presentation.activeView === "list"}
+                  onChange={checkOnlyOne} 
+                />
                 <span className={`${s.custom_radio} ${s.module}`} title="Список"></span>
                 <span className="visually-hidden">Список</span>
               </label>
@@ -34,14 +115,26 @@ export default function CatalogOptions() {
           <ul>
             <li>
               <label>
-                <input className="visually-hidden" type="checkbox" name="category" value="Кольца"/>
+                <input 
+                  className="visually-hidden"
+                  type="checkbox"
+                  name="rings"
+                  checked={category.rings}
+                  onChange={handleChange}
+                />
                 <span className={s.custom_checkbox}></span>
                 <span>Кольца</span>
               </label>
             </li>
             <li>
               <label>
-                <input className="visually-hidden" type="checkbox" name="category" value="Изделия из бисера"/>
+                <input
+                  className="visually-hidden"
+                  type="checkbox"
+                  name="beads"
+                  checked={category.beads}
+                  onChange={handleChange}
+                />
                 <span className={s.custom_checkbox}></span>
                 <span>Изделия из бисера</span>
               </label>
@@ -51,9 +144,28 @@ export default function CatalogOptions() {
 
         <fieldset className={s.budget}>
           <legend>Цена</legend>
+
+          {/* 
           <RangeSlider />
-          <input type="text" placeholder="от" pattern="[+]?(?<!\.)\b[0-9]+\b(?!\.[0-9])"/>
-          <input type="text" placeholder="до" pattern="[+]?(?<!\.)\b[0-9]+\b(?!\.[0-9])" style={{float: "right"}}/>
+           */}
+
+          <input
+            type="text"
+            placeholder="от"
+            pattern="[+]?(?<!\.)\b[0-9]+\b(?!\.[0-9])"
+            name="min"
+            value={budget.min}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            placeholder="до"
+            pattern="[+]?(?<!\.)\b[0-9]+\b(?!\.[0-9])"
+            style={{float: "right"}}
+            name="max"
+            value={budget.max}
+            onChange={handleChange}
+          />
         </fieldset>
 
         <fieldset className={s.sort}>
@@ -63,12 +175,24 @@ export default function CatalogOptions() {
               <p>Названию</p>
               <div>
                 <label>
-                  <input className="visually-hidden" type="radio" name="sort" value="name-ascending"/>
+                  <input
+                    className="visually-hidden"
+                    type="radio"
+                    name="name-ascending"
+                    checked={sort.activeSort === "name-ascending"}
+                    onChange={checkOnlyOne}
+                  />
                   <span className={`${s.custom_arrow} ${s.ascending}`} title="по возрастанию"></span>
                   <span className="visually-hidden">Возрастанию</span>
                 </label>
                 <label>
-                  <input className="visually-hidden" type="radio" name="sort" value="name-descending"/>
+                  <input
+                    className="visually-hidden"
+                    type="radio"
+                    name="name-descending"
+                    checked={sort.activeSort === "name-descending"}
+                    onChange={checkOnlyOne}
+                  />
                   <span className={`${s.custom_arrow} ${s.descending}`} title="по убыванию"></span>
                   <span className="visually-hidden">Убыванию</span>
                 </label>
@@ -78,12 +202,24 @@ export default function CatalogOptions() {
               <p>Цене</p>
               <div>
                 <label>
-                  <input className="visually-hidden" type="radio" name="sort" value="price-ascending"/>
+                  <input
+                    className="visually-hidden"
+                    type="radio"
+                    name="price-ascending"
+                    checked={sort.activeSort === "price-ascending"}
+                    onChange={checkOnlyOne}
+                  />
                   <span className={`${s.custom_arrow} ${s.ascending}`} title="по возрастанию"></span>
                   <span className="visually-hidden">Возрастанию</span>
                 </label>
                 <label>
-                  <input className="visually-hidden" type="radio" name="sort" value="price-descending"/>
+                  <input
+                    className="visually-hidden"
+                    type="radio"
+                    name="price-descending"
+                    checked={sort.activeSort === "price-descending"}
+                    onChange={checkOnlyOne}
+                  />
                   <span className={`${s.custom_arrow} ${s.descending}`} title="по убыванию"></span>
                   <span className="visually-hidden">Убыванию</span>
                 </label>
--- src/components/Catalog/Products/ProductsList.jsx
+++ src/components/Catalog/Products/ProductsList.jsx
@@ -7,6 +7,7 @@ import ProductString from './ProductString';
 export default function ProductsList({items}) {
   return (
     <ul className={s.products_list}>
+      {/* Вроде во второй ДЗ было о том, как правильно обрабатывать подобную ситуацию */}
       {items.map(item => <ProductCard item={item} key={item.id}/>)}
       {/* {items.map(item => <ProductString item={item} key={item.id}/>)} */}
     </ul>
