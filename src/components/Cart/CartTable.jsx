2eb9da84bee10f1ac2d0565f0abb3be472ee332e
Utavegu <utavegu@list.ru>
2021-04-30 22:47:09 +0500

    Реализовал отображение добавленных в корзину товаров и кнопку очистки корзины

--- src/components/Cart/CartTable.jsx
+++ src/components/Cart/CartTable.jsx
@@ -1,16 +1,47 @@
 import React, { useContext } from 'react';
 import s from './CartTable.module.css';
 import PropTypes from 'prop-types';
-import CartContext from '../../contexts/CartContext'
+import CartContext from '../../contexts/CartContext';
+import { Link } from 'react-router-dom';
+
+function TableRow({item, index}) {
+  return (
+    <tr>
+      <th scope="row" style={{border: "none"}}>{index + 1}</th>
+      <td className={s.product_name}>
+        <Link to={`/${item.id}`}>{item.name}</Link>
+      </td>
+      <td>{item.size}</td>
+      <td>{item.quantity}</td>
+      <td>{item.price} руб.</td>
+      <td>{item.price * item.quantity} руб.</td>
+      <td className={s.actions}>
+        <button className={s.button_add} title="Добавить">
+          <span className="visually-hidden">Добавить</span>
+        </button>
+        <button className={s.button_subtract} title="Убавить">
+          <span className="visually-hidden">Убавить</span>
+        </button>
+        <button className={s.button_remove} title="Удалить позицию">
+          <span className="visually-hidden">Удалить позицию</span>
+        </button>
+      </td>
+    </tr>
+  )
+
+  /* СЮДА ТОЖЕ ПРОПТАЙПСЫ */
+}
 
 function CartTable(props) {
-  const {cart} = useContext(CartContext);
-  console.log(cart);
+  const {cart, addToCart} = useContext(CartContext);
 
-  /*
-  "Очистить корзину" сразу реализуй, чтобы каждый раз не париться с очисткой истории браузера
-  И оберни в линки названия товара
-  */
+  const total = cart
+    .map(elem => elem.price * elem.quantity)
+    .reduce((sum, elem) => sum + elem, 0);
+
+  const handleClearCart = () => {
+    addToCart([]);
+  }
 
   return (
     <table className={s.estimate}>
@@ -29,36 +60,16 @@ function CartTable(props) {
       <tfoot>
         <tr>
           <td colSpan="5" style={{textAlign: "right"}}>Общая стоимость:</td>
-          <td>22 000 руб.</td>
+          <td>{total} руб.</td>
           <td>
-            <button className={s.button_clear} title="Очистить корзину" style={{width: "100%"}}>
+            <button className={s.button_clear} onClick={handleClearCart} title="Очистить корзину">
               <span className="visually-hidden">Очистить корзину</span>
             </button>
           </td>
         </tr>
       </tfoot>
       <tbody>
-
-        <tr>
-          <th scope="row" style={{border: "none"}}>1</th>
-          <td className={s.product_name}>Модное дофига кольцо</td>
-          <td>23.5</td>
-          <td>3</td>
-          <td>2 000 руб.</td>
-          <td>6 000 руб.</td>
-          <td className={s.actions}>
-            <button className={s.button_add} title="Добавить">
-              <span className="visually-hidden">Добавить</span>
-            </button>
-            <button className={s.button_subtract} title="Убавить">
-              <span className="visually-hidden">Убавить</span>
-            </button>
-            <button className={s.button_remove} title="Удалить позицию">
-              <span className="visually-hidden">Удалить позицию</span>
-            </button>
-          </td>
-        </tr>
-
+        {cart.map((product, id) => <TableRow item={product} index={id} key={product.name + product.size} />)}
       </tbody>
     </table>
   )
