9456b4c718443f433277dd3d6ec52da57eabf74f
Utavegu <utavegu@list.ru>
2021-05-01 18:50:34 +0500

    Реализовал возможность управления количеством товара из корзины

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import s from './Cart.module.css';
 import CartTable from './CartTable';
+import CartContext from '../../contexts/CartContext';
+import { Link } from 'react-router-dom';
 
 export default function Cart() {
+  const {cart, addToCart} = useContext(CartContext);
+  const emptyCartStyle = {
+    textAlign: "center",
+    fontSize: "larger",
+    margin: 0,
+    padding: "100px",
+  }
 
   return (
     
     <section className={s.cart}>
       <h2 className="visually-hidden">Корзина</h2>
       <section>
-        <CartTable />
+        {cart.length
+        ?
+        <CartTable cart={cart} addToCart={addToCart} />
+        :
+        <p style={emptyCartStyle}>В корзине пусто! Чтобы помочь корзине победить пустоту внутри - пригляди себе что-нибудь в <Link to="/" style={{color: "inherit", textDecoration: "underline"}}>каталоге</Link></p>}
       </section>
 
     {/*     
--- src/components/Cart/CartTable.jsx
+++ src/components/Cart/CartTable.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useContext } from 'react';
 import s from './CartTable.module.css';
 import PropTypes from 'prop-types';
-import CartContext from '../../contexts/CartContext';
 import { Link } from 'react-router-dom';
+import productsData from '../../data/products.json';
 
-function TableRow({item, orderNumber, onRemove: handleRemove, onAdd: handleAdd}) {
+function TableRow({item, orderNumber, onRemove: handleRemove, onChangeQuantity: handleChangeQuantity}) {
   return (
     <tr>
       <th scope="row" style={{border: "none"}}>{orderNumber + 1}</th>
@@ -19,7 +19,7 @@ function TableRow({item, orderNumber, onRemove: handleRemove, onAdd: handleAdd})
 
         <button
           className={s.button_add}
-          onClick={() => handleAdd(orderNumber)}
+          onClick={(evt) => handleChangeQuantity(evt, orderNumber)}
           title="Добавить"
         >
           <span className="visually-hidden">Добавить</span>
@@ -27,6 +27,7 @@ function TableRow({item, orderNumber, onRemove: handleRemove, onAdd: handleAdd})
 
         <button
           className={s.button_subtract}
+          onClick={(evt) => handleChangeQuantity(evt, orderNumber)}
           title="Убавить"
         >
           <span className="visually-hidden">Убавить</span>
@@ -47,9 +48,7 @@ function TableRow({item, orderNumber, onRemove: handleRemove, onAdd: handleAdd})
   /* СЮДА ТОЖЕ ПРОПТАЙПСЫ */
 }
 
-function CartTable(props) {
-  const {cart, addToCart} = useContext(CartContext);
-
+function CartTable({cart, addToCart}) {
   const total = cart
     .map(elem => elem.price * elem.quantity)
     .reduce((sum, elem) => sum + elem, 0);
@@ -62,10 +61,15 @@ function CartTable(props) {
     addToCart(cart.filter((_, id) => id !== orderNumber));
   }
 
-  const handleAdd = orderNumber => {
-    let test = cart.find((_, id) => id === orderNumber);
-    test.quantity += 1;
-    addToCart(cart);
+  const handleChangeQuantity = ({target}, orderNumber) => {
+    const newCart = cart.slice();
+    const targetPosition = newCart.find((_, id) => id === orderNumber);
+    // Не самое изящное решение на случай мультиязычного интерфейса, подумай как лучше... вот если бы там был плюс... Но я хз на сколько это семантично и доступно
+    (target.title === "Добавить") ? targetPosition.quantity += 1 : targetPosition.quantity -= 1;
+    const totalExemplars = productsData[targetPosition.id].sizes.find(o => o.size === targetPosition.size).quantity;
+    if (targetPosition.quantity > totalExemplars) targetPosition.quantity = totalExemplars;
+    if (targetPosition.quantity < 1) targetPosition.quantity = 1;
+    addToCart(newCart);
   }
 
   return (
@@ -99,7 +103,8 @@ function CartTable(props) {
           orderNumber={id}
           key={product.name + product.size}
           onRemove={handleRemove}
-          onAdd={handleAdd}
+          onChangeQuantity={handleChangeQuantity}
+          quantity={product.quantity}
         />)}
       </tbody>
     </table>
